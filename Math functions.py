b9c11bd88fb338dba9fe526538adf64d7cc6087a
RishiWig3 <54427319+RishiWig3@users.noreply.github.com>
2019-08-23 13:59:51 +1000

    Add files via upload

---
+++ Math functions.py
@@ -0,0 +1,61 @@
+import math
+
+def areaCircle(radius):
+    Circlearea = float(math.pi * (radius**2))
+    return Circlearea
+
+def volumeCylinder(radius, height):
+    CylinderVolume = float(((math.pi * (radius**2)) * height))
+    return CylinderVolume
+
+def areaTriangle(base, height):
+    TriangleArea = float((base * height) * 0.5)
+    return TriangleArea
+
+def areaSquare(side):
+    SquareArea = float(side**2)
+    return SquareArea
+
+def volumeCube(side):
+    CubeVolume = float(side**3)
+    return CubeVolume
+
+def rectangleArea(length, width):
+    AreaRectangle = float(length * width)
+    return AreaRectangle
+
+def RectangularPrismVolume(length, width, breadth):
+    VolumeRectangularPrism = float(length*width*breadth)
+    return VolumeRectangularPrism
+
+def Quadratic(a, b, c):
+    QuadraticEquation2 = float(((-b) - (((b**2)-4*a*c)**0.5)/(2*a))
+    return QuadraticEquation2
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                      
+
+
+
+
+    
+
